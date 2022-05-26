d9174c182fc42d8790a9bede966968d6144bcfab
Arman Maghsoudnia <arman.ma79@gmail.com>
2022-05-26 10:29:18 +0430

    The Python code HTTP_Server.py is modified. Some features added.

--- Problem_3/HTTP_Server.py
+++ Problem_3/HTTP_Server.py
@@ -14,7 +14,7 @@ class handler(BaseHTTPRequestHandler):
                     self.end_headers()
                     self.wfile.write(fh.read().encode())
             else:
-                self.send_error(404,'File Not Found: %s' % self.path)
+                self.send_error(405,'This is NOT Allowed in our Web Server. You can only GET this path: /api/v1/status/status.json')
         except IOError:
             self.send_error(404,'File Not Found: %s' % self.path)
 
@@ -22,14 +22,16 @@ class handler(BaseHTTPRequestHandler):
         if self.path == '/api/v1/status' or self.path == '/api/v1/status/status.json':
             length = self.headers['content-length']
             data = self.rfile.read(int(length))
-
             with open(self.store_path, 'w') as fh:
                 fh.write(data.decode() + '\n')
+            self.send_response(201)
+            self.send_header('Content-type','text/html')
+            self.end_headers()
+            self.wfile.write(bytes(data.decode() + '\n', "utf8"))
+        else:
+            self.send_error(405,'This is NOT Allowed in our Web Server. You can only POST to this path: /api/v1/status/status.json')
 
-        self.send_response(201)
-        self.send_header('Content-type','text/html')
-        self.end_headers()
-        self.wfile.write(bytes(data.decode() + '\n', "utf8"))
+
 
 with HTTPServer(('', 8000), handler) as server:
     server.serve_forever()
